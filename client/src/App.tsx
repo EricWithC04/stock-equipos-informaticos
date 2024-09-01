a6373e90a25d4b3afad42cb3c8a35f1e322634e0
EricWithC04 <negativegame117@gmail.com>
2024-09-01 11:10:26 -0300

    feat: routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,18 @@
+import { Routes, Route } from 'react-router-dom'
+import Stock from './pages/Stock/Stock'
+import NewEquipments from './pages/NewEquipments/NewEquipments'
+import Brands from './pages/Brands/Brands'
+import Categories from './pages/Categories/Categories'
 import './App.css'
 
 function App() {
   return (
-    <>
-      <h1>Hola</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<Stock />} />
+      <Route path="/register-equipment" element={<NewEquipments />} />
+      <Route path="/brands" element={<Brands />} />
+      <Route path="/categories" element={<Categories />} />
+    </Routes>
   )
 }
 
---
+++ client/src/pages/NewEquipments/NewEquipments.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewEquipments = () => {
+    return (
+        <div>NewEquipments</div>
+    )
+}
+
+export default NewEquipments
