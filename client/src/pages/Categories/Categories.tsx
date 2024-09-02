2693f01e4d837731c8a339def13e9cb6a6911e1c
EricWithC04 <negativegame117@gmail.com>
2024-09-01 21:42:02 -0300

    feat: list of categories

--- client/src/pages/Categories/Categories.tsx
+++ client/src/pages/Categories/Categories.tsx
@@ -1,9 +1,42 @@
+import { useState, useEffect } from "react"
 import NavBar from "../../components/NavBar/NavBar"
+import styles from "./Categories.module.css"
+
+interface ICategory {
+    name: string
+}
 
 const Categories = () => {
+
+    const [categories, setCategories] = useState<Array<ICategory>>([])
+
+    useEffect(() => {
+        fetch("http://localhost:3000/api/category")
+            .then((res) => res.json())
+            .then((data) => setCategories(data))
+            .catch((err) => console.log(err))
+    }, [])
+
     return (
         <NavBar>
-            <div>Categories</div>
+            <div className={styles["container"]}>
+                <h2>Gestionar Categorias</h2>
+                <form className={styles["category-form"]}>
+                    <input type="text" placeholder="Nueva Categoría"/>
+                    <button type="submit">Agregar Categoría</button>
+                </form>
+                <div className={styles["categories-container"]}>
+                    {
+                        categories.map((category) => {
+                            return (
+                                <h3>
+                                    {category.name}
+                                </h3>
+                            )
+                        })
+                    }
+                </div>
+            </div>
         </NavBar>
     )
 }
