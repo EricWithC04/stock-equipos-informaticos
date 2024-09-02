ea505f60a7006dc630eeb6d188ef074e63249fb2
EricWithC04 <negativegame117@gmail.com>
2024-09-01 22:18:37 -0300

    feat: form for new equipment

--- client/src/pages/NewEquipments/NewEquipments.tsx
+++ client/src/pages/NewEquipments/NewEquipments.tsx
@@ -1,9 +1,39 @@
 import NavBar from "../../components/NavBar/NavBar"
+import styles from "./NewEquipments.module.css"
 
 const NewEquipments = () => {
     return (
         <NavBar>
-            <div>NewEquipments</div>
+            <div className={styles["container"]}>
+                <h2>Agregar Nuevo Equipo</h2>
+                <form className={styles["equipment-form"]}>
+                    <select defaultValue={"Tipo"}>
+                        <option value="Tipo">Tipo</option>
+                        <option value="Notebook">Notebook</option>
+                        <option value="Impresora">Impresora</option>
+                        <option value="Router">Router</option>
+                    </select>
+                    <select defaultValue={"Marca"}>
+                        <option value="Marca">Marca</option>
+                        <option value="Notebook">Asus</option>
+                        <option value="Impresora">HP</option>
+                        <option value="Router">Dell</option>
+                    </select>
+                    <input 
+                        type="text" 
+                        placeholder="Modelo"
+                    />
+                    <input 
+                        type="text" 
+                        placeholder="Número de Serie"
+                    />
+                    <input 
+                        type="number" 
+                        placeholder="Cantidad"
+                    />
+                    <button type="submit">Agregar</button>
+                </form>
+            </div>
         </NavBar>
     )
 }
