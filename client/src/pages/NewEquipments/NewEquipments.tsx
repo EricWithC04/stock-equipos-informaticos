39e3837318af41a146e54abaa3aeeadf2090a379
EricWithC04 <negativegame117@gmail.com>
2024-09-01 22:33:02 -0300

    feat: form for new equipment connected with the server

--- client/src/pages/NewEquipments/NewEquipments.tsx
+++ client/src/pages/NewEquipments/NewEquipments.tsx
@@ -1,39 +1,144 @@
+import React, { useState, useEffect } from "react"
+import { useNavigate } from "react-router-dom"
 import NavBar from "../../components/NavBar/NavBar"
 import styles from "./NewEquipments.module.css"
 
+interface IBrand {
+    id: number
+    name: string
+}
+
+interface ICategory {
+    id: number
+    name: string
+}
+
+interface INewEquipment {
+    model: string
+    serial: string
+    stock: number
+    brandId: number
+    categoryId: number
+}
+
 const NewEquipments = () => {
+
+    const navigate = useNavigate()
+
+    const [brands, setBrands] = useState<Array<IBrand>>([])
+    const [categories, setCategories] = useState<Array<ICategory>>([])
+    const [newEquipment, setNewEquipment] = useState<INewEquipment>({
+        model: "",
+        serial: "",
+        stock: 0,
+        brandId: 0,
+        categoryId: 0
+    })
+
+    useEffect(() => {
+        fetch("http://localhost:3000/api/brand")
+            .then((res) => res.json())
+            .then((data) => setBrands(data))
+            .catch((err) => console.log(err))
+        
+        fetch("http://localhost:3000/api/category")
+            .then((res) => res.json())
+            .then((data) => setCategories(data))
+            .catch((err) => console.log(err))
+    }, [])
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNewEquipment({
+            ...newEquipment,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setNewEquipment({
+            ...newEquipment,
+            [e.target.name]: parseInt(e.target.value)
+        })
+    }
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault()
+
+        fetch("http://localhost:3000/api/equipment", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newEquipment)
+        })
+            .then((_res) => navigate("/"))
+            .catch((err) => console.log(err))
+        
+        setNewEquipment({
+            model: "",
+            serial: "",
+            stock: 0,
+            brandId: 0,
+            categoryId: 0
+        })
+    }
+
     return (
         <NavBar>
-            <div className={styles["container"]}>
-                <h2>Agregar Nuevo Equipo</h2>
-                <form className={styles["equipment-form"]}>
-                    <select defaultValue={"Tipo"}>
-                        <option value="Tipo">Tipo</option>
-                        <option value="Notebook">Notebook</option>
-                        <option value="Impresora">Impresora</option>
-                        <option value="Router">Router</option>
-                    </select>
-                    <select defaultValue={"Marca"}>
-                        <option value="Marca">Marca</option>
-                        <option value="Notebook">Asus</option>
-                        <option value="Impresora">HP</option>
-                        <option value="Router">Dell</option>
-                    </select>
-                    <input 
-                        type="text" 
-                        placeholder="Modelo"
-                    />
-                    <input 
-                        type="text" 
-                        placeholder="Número de Serie"
-                    />
-                    <input 
-                        type="number" 
-                        placeholder="Cantidad"
-                    />
-                    <button type="submit">Agregar</button>
-                </form>
-            </div>
+            {
+                brands.length && categories.length ? (
+                    <div className={styles["container"]}>
+                        <h2>Agregar Nuevo Equipo</h2>
+                        <form className={styles["equipment-form"]} onSubmit={handleSubmit}>
+                            <select defaultValue={""} name="categoryId" onChange={handleSelect}>
+                                <option value="">Tipo</option>
+                                {
+                                    categories.map((category) => (
+                                        <option 
+                                            key={category.id} 
+                                            value={category.id}
+                                        >
+                                            {category.name}
+                                        </option>
+                                    ))
+                                }
+                            </select>
+                            <select defaultValue={""} name="brandId" onChange={handleSelect}>
+                                <option value="">Marca</option>
+                                {
+                                    brands.map((brand) => (
+                                        <option
+                                            key={brand.id}
+                                            value={brand.id}
+                                        >
+                                            {brand.name}
+                                        </option>
+                                    ))
+                                }
+                            </select>
+                            <input 
+                                type="text" 
+                                placeholder="Modelo"
+                                name="model"
+                                onChange={handleChange}
+                            />
+                            <input 
+                                type="text" 
+                                placeholder="Número de Serie"
+                                name="serial"
+                                onChange={handleChange}
+                            />
+                            <input 
+                                type="number" 
+                                placeholder="Cantidad"
+                                name="stock"
+                                onChange={handleChange}
+                            />
+                            <button type="submit">Agregar</button>
+                        </form>
+                    </div>
+                ) : <h1>Cargando...</h1>
+            }
         </NavBar>
     )
 }
