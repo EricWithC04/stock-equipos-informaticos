9da8aa80f43c9255a6a7925a8473640360e7228a
EricWithC04 <negativegame117@gmail.com>
2024-09-01 21:54:44 -0300

    feat: create a new brand

--- client/src/pages/Brands/Brands.tsx
+++ client/src/pages/Brands/Brands.tsx
@@ -10,6 +10,7 @@ const Brands = () => {
 
     const [brands, setBrands] = useState<Array<IBrand>>([])
 
+    const [newBrand, setNewBrand] = useState<string>("")
 
     useEffect(() => {
         fetch("http://localhost:3000/api/brand")
@@ -18,15 +19,40 @@ const Brands = () => {
             .catch((err) => console.log(err))
     }, [])
 
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNewBrand(e.target.value)
+    }
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        fetch("http://localhost:3000/api/brand", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ name: newBrand })
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                setBrands([...brands, data])
+                setNewBrand("")
+            })
+            .catch((err) => {
+                console.log(err)
+        })
+    }
+
     return (
         <NavBar>
             <div className={styles["container"]}>
                 <h2>Gestionar Categorias</h2>
-                <form className={styles["brand-form"]} >
+                <form className={styles["brand-form"]} onSubmit={handleSubmit}>
                     <input 
                         type="text" 
                         placeholder="Nueva Marca"
-                        
+                        value={newBrand}
+                        onChange={handleChange}
                     />
                     <button type="submit">Agregar Marca</button>
                 </form>
