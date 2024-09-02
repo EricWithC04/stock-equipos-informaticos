6bc63ab59b60a71e812ee14f85e5587133c9580a
EricWithC04 <negativegame117@gmail.com>
2024-09-01 21:48:01 -0300

    feat: create new category

--- client/src/pages/Categories/Categories.tsx
+++ client/src/pages/Categories/Categories.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react"
 import NavBar from "../../components/NavBar/NavBar"
 import styles from "./Categories.module.css"
 
@@ -10,6 +10,8 @@ const Categories = () => {
 
     const [categories, setCategories] = useState<Array<ICategory>>([])
 
+    const [newCategory, setNewCategory] = useState<string>("")
+
     useEffect(() => {
         fetch("http://localhost:3000/api/category")
             .then((res) => res.json())
@@ -17,12 +19,41 @@ const Categories = () => {
             .catch((err) => console.log(err))
     }, [])
 
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNewCategory(e.target.value)
+    }
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        fetch("http://localhost:3000/api/category", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ name: newCategory })
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                setCategories([...categories, data])
+                setNewCategory("")
+            })
+            .catch((err) => {
+                console.log(err)
+        })
+    }
+
     return (
         <NavBar>
             <div className={styles["container"]}>
                 <h2>Gestionar Categorias</h2>
-                <form className={styles["category-form"]}>
-                    <input type="text" placeholder="Nueva Categoría"/>
+                <form className={styles["category-form"]} onSubmit={handleSubmit}>
+                    <input 
+                        type="text" 
+                        placeholder="Nueva Categoría"
+                        value={newCategory}
+                        onChange={handleChange}
+                    />
                     <button type="submit">Agregar Categoría</button>
                 </form>
                 <div className={styles["categories-container"]}>
