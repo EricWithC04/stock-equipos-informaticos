e851d68f1e87b5cf44548dc1eb5913bf4b64a2e3
EricWithC04 <negativegame117@gmail.com>
2024-09-09 17:16:04 -0300

    feat: migrate db to postgres

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -10,11 +10,13 @@ const {
     DIALECT
 } = env.DB
 
-const sequelize = new Sequelize(NAME, USER, PASSWORD, {
-    host: HOST,
-    port: parseInt(PORT),
-    dialect: DIALECT,
-    logging: false
-})
+// const sequelize = new Sequelize(NAME, USER, PASSWORD, {
+//     host: HOST,
+//     port: parseInt(PORT),
+//     dialect: DIALECT,
+//     logging: false
+// })
+
+const sequelize = new Sequelize(`${DIALECT}://${USER}:${PASSWORD}@${HOST}:${PORT}/${NAME}`)
 
 export { sequelize }
