a4d78c79b7a82e7dc6fb6062fed4750ebbcd1b28
EricWithC04 <negativegame117@gmail.com>
2024-09-15 12:01:02 -0300

    feat: pages for login and register

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,11 +5,15 @@ import Brands from './pages/Brands/Brands'
 import Categories from './pages/Categories/Categories'
 import './App.css'
 import Landing from './pages/Landing/Landing'
+import Login from './pages/Login/Login'
+import Register from './pages/Register/Register'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Landing />}/>
+      <Route path="/login" element={<Login />}/>
+      <Route path="/register" element={<Register />}/>
       <Route path="/stock" element={<Stock />} />
       <Route path="/register-equipment" element={<NewEquipments />} />
       <Route path="/brands" element={<Brands />} />
--- client/src/pages/Landing/Landing.tsx
+++ client/src/pages/Landing/Landing.tsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import styles from './Landing.module.css'
 
 const Landing = () => {
+
+    const navigate = useNavigate()
+
     return (
         <div className={styles["container"]}>
             <h1>Stock Equipos Informaticos</h1>
             <p>Simplifica la gestión de tus equipos informáticos con nuestra plataforma intuitiva y eficiente.</p>
             <div className={styles["buttons-container"]}>
-                <button className={styles["login"]}>
+                <button className={styles["login"]} onClick={() => navigate('/login')}>
                     Iniciar Sesión
                 </button>
-                <button className={styles["register"]}>
+                <button className={styles["register"]} onClick={() => navigate('/register')}>
                     Registrarse
                 </button>
             </div>
